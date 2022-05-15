9e70fcbb855d3a0b6ace09bf65cd297e1383d2ff
hosam waheed hosny <hosam4waheed@gmail.com>
2022-05-16 00:02:24 +0200

    Add Search bar

--- main.js
+++ main.js
@@ -4,14 +4,24 @@ var age = document.getElementById('age');
 var btn = document.getElementById('btn');
 var toDolist = document.getElementById('to-do-list');
 var msg = document.getElementById('msg');
-var id = 1;
+var search = document.getElementById('search');
+var globalId = 1;
+var listArr = [];
 
-//Delete Items From To Do list
+//Delete Function
 function del(id) {
     var listItems = toDolist.querySelectorAll('li');
+    //remove from Dom
     for(var i = 0; i < listItems.length; i++){
         if(listItems[i].id == id){
             listItems[i].remove();
+        }
+    }
+
+    //remove from Arr
+    for(var i = 0; i < listArr.length; i++){
+        if(listArr[i].id == id){
+            listArr.splice(listArr[i],1);
         }
     }
 }
@@ -20,22 +30,23 @@ function del(id) {
 function add(){
     msg.style.display = "none";
     //Remove Spacing
-    var userValue = user.value.trim();
+    var userValue = user.value.toLowerCase().trim();
     //Convert To Number
     var ageNumber = parseInt(age.value);
 
     //Check User Name && age Number
-    if(userValue && ageNumber){
+    if(userValue && ageNumber >= 0){
 
         //Check Condition Of Input Data 
         if(userValue.length <= 10 &&
             (ageNumber > 0 && ageNumber <= 100)){     
                 toDolist.innerHTML += 
                 '<li ' +
-                    'id=' + id.toString() + '><span>User Name:</span>' + userValue + '<span> Age:</span>' + age.value +
-                    "<button onclick=del(" + id + ")>Delete</button>" + 
+                    'id=' + globalId.toString() + '><span>User Name:</span>' + userValue + '<span> Age:</span>' + age.value +
+                    "<button onclick=del(" + globalId + ")>Delete</button>" + 
                 '</li>';
-                id += 1;
+                listArr.push({id:globalId, name:userValue, age:age.value });
+                globalId += 1;
                 user.value = "";
                 age.value = "";
 
@@ -45,7 +56,7 @@ function add(){
                 msg.innerHTML = 'Enter User Name Less Than 10 Letters';
 
             }else if(userValue.length <= 10 &&
-                (ageNumber < 0 || ageNumber > 100)){
+                (ageNumber <= 0 || ageNumber > 100)){
                 msg.style.display = "block";
                 msg.innerHTML = 'Enter User Age From 1 To 100';
 
@@ -70,3 +81,18 @@ function add(){
 
 //OnClick Event
 btn.onclick = add;
+
+//Search Function
+search.onkeyup = function(){
+    var searchValue = search.value.toLowerCase().trim();
+    var newlist = [];
+        for(var i of listArr){
+            if(i.name.includes(searchValue)){
+                newlist.push('<li ' +
+                'id=' + i.id.toString() + '><span>User Name:</span>' + i.name + '<span> Age:</span>' + i.age +
+                "<button onclick=del(" + i.id + ")>Delete</button>" + 
+            '</li>')
+            }
+    }
+    toDolist.innerHTML = newlist.join("");
+}
